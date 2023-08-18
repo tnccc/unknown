5545a9e0f4cfa748e540777255a21148d80f0a00
tnccc <irei.frontend@gmail.com>
2023-08-18 18:09:55 +0900

    fixed

--- src/components/common/CommonCardList.tsx
+++ src/components/common/CommonCardList.tsx
@@ -1,6 +1,16 @@
 import classes from '@/styles/common/card_list.module.scss'
 import { CommonCard } from "./CommonCard"
-import { Item } from '../../../public/const/allItems';
+
+type Item = {
+  id: string;
+  name: string;
+  price: number;
+  department: string;
+  category: string;
+  path: string;
+  released_at: string;
+  sizes: string[];
+}
 
 type CommonCardListProps = {
   items: Item[];
